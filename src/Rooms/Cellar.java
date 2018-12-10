7618c2cf98402b5f9427420c3a71d9aa593c64d7
BT_1N3_15 <dfs>
2018-12-10 14:46:10 -0500

    i big like turtles

--- src/Rooms/Cellar.java
+++ src/Rooms/Cellar.java
@@ -1,7 +1,17 @@
 package Rooms;
 
+import People.Person;
+
 public class Cellar extends Room {
     public Cellar(int x, int y) {
-        super(x , y);
+        super(x, y);
+    }
+
+
+    public void enterRoom(Person x) {
+        System.out.println(" You have entered the dungeon. You look around and see cobwebs in every corner, spiders and all types of insects wander the room. Spread across the floor is a small collection of human bones and ripped clothes.");
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
     }
 }
--- src/Rooms/Solar.java
+++ src/Rooms/Solar.java
@@ -1,12 +1,18 @@
 package Rooms;
 
 
+import People.Person;
 
-public class Solar extends Room{
+public class Solar extends Room {
     public Solar(int x, int y) {
-        super(x , y);
+        super(x, y);
     }
 
-
+    public void enterRoom(Person x) {
+        System.out.println(" You enter a grand suite. You look left and see an elaborately decorated living room adorned with gold furnished chairs and sofa's. The walls are lined with elaborate ");
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
     }
 
+}
